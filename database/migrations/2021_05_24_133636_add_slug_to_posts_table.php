529d2c25a3a667e60c2c517e34a4bdf52acb1285
giordanoale <a.giordano89@gmail.com>
2021-05-24 15:39:32 +0200

    Aggiunta colonna Slug alla tabella e tutte le colonne alla tabella Comments

--- database/migrations/2021_05_24_133636_add_slug_to_posts_table.php
+++ database/migrations/2021_05_24_133636_add_slug_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class AddSlugToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('posts_id')->constrained();
-            $table->string('name', 100)->nullable();
-            $table->text('content');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('slug')->unique();
         });
     }
 
@@ -29,6 +25,8 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
     }
 }
